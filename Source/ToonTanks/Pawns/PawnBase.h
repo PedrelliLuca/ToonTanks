1c29783900db45397ae6cbdfd36dcc377aa88e03
Luca Pedrelli <pedrelli.luca@gmail.com>
2020-07-11 23:12:02 +0200

    :beginner: PawnTank c++ class created. Unique components added.

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -19,30 +19,20 @@ private:
 
 	// The capsule components will act as root component and the general collision for the pawns
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UCapsuleComponent* CapsuleComp;
+	UCapsuleComponent* CapsuleComp = nullptr;
 
 	// One mesh component is the base mesh, one is the turret mesh. This will allow them to rotate independently
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* BaseMesh;
+	UStaticMeshComponent* BaseMesh = nullptr;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	UStaticMeshComponent* TurretMesh;
-
+	UStaticMeshComponent* TurretMesh = nullptr;
 
 	// Scene component to act as the projectile spawn point7
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
-	USceneComponent* ProjectileSpawnPoint;
+	USceneComponent* ProjectileSpawnPoint = nullptr;
 
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
 
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 };
--- notes.md
+++ notes.md
@@ -1,10 +1,10 @@
 # Project Notes
 
-### Lesson 135
+### Lesson 135 - Creating components
 
 All the attributes with the U prefix will need the `UPROPERTY()` macro above them
 
-### Lesson 136
+### Lesson 136 - Deriving blueprint classes
 
 We derived blueprint classes from our base class. Let's examine them:
 
@@ -15,7 +15,7 @@ Turret class has everything we need to make the enemy turret function.
 
 For the tank, since it's the pawn controlled by the player, we will add a camera component and a spring arm component, which gives us extra control on the camera.
 
-### Lesson 137
+### Lesson 137 - Access modifiers
 
 Access specifiers explained
 
@@ -24,3 +24,11 @@ For components we'll use:
 - BlueprintReadOnly (we want to see them in blueprint because they are visual components and blueprints allow us to take a look at them)
 - Category = "Components", for tidiness
 - meta = (AllowPrivateAccess = "true") because the compiler normally wouldn't allow private memebers like our components to be visible in the editor
+
+Now we have the details of the components exposed in our blueprints
+
+### Lesson 138 - Creating child C++ class
+
+The new class won't appear in the editor until you next restart the project.
+
+Let's make a PawnTank class and add a SpringArm and Camera components to it!
